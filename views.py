a9a508361fb39bc7529443e41ccbbf38c42803eb
kaisajia <fengsiqiwxm@163.com>
2017-04-18 22:52:15 +0800

    forum

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello World!")
+    block_infos=[{"name":"运维专区","desc":"运维学习讨论区","manager":"admin"},
+        {"name":"Django专区","desc":"Django学习讨论区","manager":"admin"},
+        {"name":"部落建设","desc":"有关部落建设的事宜","manager":"admin"}    
+    ]
+    return render(request,"index.html")
